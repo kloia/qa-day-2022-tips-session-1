39f0944c69818529c4eb6a491406514beb485e5d
muhammet_topcu <muhammet.topcu@kloia.com>
2022-11-11 15:34:47 +0300

    QA-Day files

---
+++ Tips Session One/tip_one.rb
@@ -0,0 +1,6 @@
+# Tip-1 Casting anything into corresponding boolean value.
+
+regex_pattern = /Welcome to the QA Day \d{4}!/
+text = "Cemal Welcome to the QA Day 2022!"
+
+puts text =~ regex_pattern
